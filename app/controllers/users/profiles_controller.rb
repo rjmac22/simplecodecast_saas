ff626e1d8f6e49b95671011440fbff00c10fcfed
Rob Maclean <rjmac@live.co.uk>
2017-10-19 23:02:32 +0100

    Added profiles new action

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,2 +1,7 @@
 class ProfilesController < ApplicationController
+    def new
+        #form where a user can fill out their own ProfilesController
+        @user = User.find( params[:user_id] )
+        @profile = @user.bulid_profile
+    end
 end
